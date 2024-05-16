c3e99b8b5fa29c86d4a63ac184367c87a6a6337a
victorvegak <vvega28@outlook.com>
2024-05-16 01:30:55 -0600

    DEPLOY ISSUES

--- database/index.js
+++ database/index.js
@@ -31,7 +31,7 @@ module.exports = {
 }
 } else {
   pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL_INTERNAL,
   })
   module.exports = pool
 }
