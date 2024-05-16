07ee90105fda13883f3593e67dea7da5682d4e84
victorvegak <vvega28@outlook.com>
2024-05-16 01:13:26 -0600

    error

--- server.js
+++ server.js
@@ -43,14 +43,15 @@ app.use(async (req, res, next) => {
 * Place after all other middleware
 *************************/
 app.use(async (err, req, res, next) => {
-  let nav = await utilities.getNav();
-  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message || 'Oh no! There was a crash. Maybe try a different',
+    message,
     nav
-  });
-});
+  })
+})
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
