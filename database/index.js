2fffb351c5304d1ac4016ad25203012e183562d0
victorvegak <vvega28@outlook.com>
2024-06-05 20:20:28 -0600

    update

--- database/index.js
+++ database/index.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV == "development") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-      rejectUnauthorized: false,
+      rejectUnauthorized: true,
     },
 })
 
